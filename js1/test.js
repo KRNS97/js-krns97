bdb66067bbb3d2eff1ca86eacd22b99cc5b4953c
KRNS97 <154898039+KRNS97@users.noreply.github.com>
2023-12-28 14:40:23 +0000

    container config done

--- js1/test.js
+++ js1/test.js
@@ -1,1 +1,15 @@
 console.log("My name is karan")
+
+const accountId = 75936
+let accountEmail = "ramesh@google.com"
+var accountPassword = "12345"
+accountCity = "Lucknow"
+
+//accountId = 5 not allowed
+
+accountEmail = "hsgdbd@gmail.com"
+accountPassword = "76945"
+accountCity = "kanpur"
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity])
