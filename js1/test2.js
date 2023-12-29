b7a035e7bc3f9fe4993f7e12caa4c7c78da3bb36
KRNS97 <154898039+KRNS97@users.noreply.github.com>
2023-12-29 13:46:13 +0000

    type convertion

---
+++ js1/test2.js
@@ -0,0 +1,18 @@
+let score = "23"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let islogedIn = 1
+
+let booleabIslogedIn = Boolean(islogedIn)
+console.log(booleabIslogedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
