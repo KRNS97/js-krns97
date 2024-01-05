8f4a2f8436a24403c0cda6cb61f0014a90dd87b8
KRNS97 <154898039+KRNS97@users.noreply.github.com>
2024-01-05 13:53:13 +0000

    saved

---
+++ js1/test3.js
@@ -0,0 +1,21 @@
+const myarr = [0, 1, 2, 3, 4]
+const Heros = ["Ironman", "batman", "moonknight"]
+
+const myarrr2 = new Array(1,2,3,4,5)
+console.log(myarr[3]);
+
+myarr.push(5)
+
+console.log(myarr)
+
+myarr.pop()
+
+console.log(myarr)
+
+myarr.unshift(7)
+
+console.log(myarr)
+
+myarr.shift()
+
+console.log(myarr)
